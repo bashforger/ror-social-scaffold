569e5bbe47ffe32d0362f19f71b720fdc17e8c52
bashforger <bash.forger@gmail.com>
2021-02-17 20:12:00 +0500

    Modified friendships_controller.rb with related methods

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,5 @@
 class FriendshipsController < ApplicationController
   include FriendshipsHelper
-  before_action :set_friendship
 
   def index
     @friendships = Friendship.all
@@ -9,37 +8,38 @@ class FriendshipsController < ApplicationController
   def show; end
 
   def new
-    @friendship = Friendship.new
   end
 
   def create
     @friendship = Friendship.new(sender_id: params[:sender], receiver_id: params[:receiver])
-
+    logger.debug "New post: #{@friendship.attributes.inspect}"
     if @friendship.save
       flash[:notice] = 'Friendship was successfully created.'
     else
-      flash[:alert] = 'Transaction failed.. Unable to create friendship!'
-    end
-    redirect_to request.referrer
-  end
-
-  def destroy_both
-    @friendship = Friendship.where(sender_id: params[:receiver], receiver_id: params[:sender]).first
-    if @friendship.destroy
-      destroy
-    else
-      flash[:alert] = 'Transaction failed.. Unable to remove friendship!'
+      flash[:alert] = 'Transaction failed.. Unable to create friendship! #{@friendship}'
     end
     redirect_to request.referrer
   end
 
   def destroy
     @friendship = Friendship.where(sender_id: params[:sender], receiver_id: params[:receiver]).first
-
     if @friendship.destroy
       flash[:notice] = 'Friendship was successfully removed.'
     else
       flash[:alert] = 'Friendship not removed!'
+    end
+    redirect_to request.referrer
+  end
+
+  def accept_request
+    flash[:notice] = 'Update route hit successfully'
+    @friendship = Friendship.where( sender_id: params[:sender], receiver_id: params[:receiver] ).first
+    @friendship.status = true if @friendship
+    if @friendship && (@friendship.save)
+      logger.debug 'yeah: #{Friendship.all} \n #{sender} \n #{receiver}'
+      flash[:notice] = 'Added as friend successfully'
+    else
+      flash[:alert] = 'Something went wrong successfully'
     end
     redirect_to request.referrer
   end
