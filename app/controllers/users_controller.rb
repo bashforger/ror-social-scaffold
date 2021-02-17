1b6c4ba1298a3a2a97b15d88730f77e0faeca28e
bashforger <bash.forger@gmail.com>
2021-02-17 20:15:24 +0500

    Modified User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+  include UserHelper
   before_action :authenticate_user!
+  before_action :set_user, :set_friendship, only: [:show]
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    set_friendship
     @posts = @user.posts.ordered_by_most_recent
   end
 end
